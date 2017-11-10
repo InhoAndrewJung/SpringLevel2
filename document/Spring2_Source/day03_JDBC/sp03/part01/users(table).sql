7a95b46ba60341ae958c80c02e530dcb3f04874f
InhoAndrewJung <inho.jeoung@gmail.com>
2017-11-10 19:29:24 +0900

    add document 20171110

---
+++ document/Spring2_Source/day03_JDBC/sp03/part01/users(table).sql
@@ -0,0 +1,38 @@
+==========================
+ 회원  table 생성 및 data insert
+==========================
+
+==========================
+Drop Table
+==========================
+DROP TABLE users;
+
+
+-------------------------------------------------------------------------
+DROP TABLE users purge;
+CREATE TABLE users ( 
+	user_id		VARCHAR2(7)	PRIMARY KEY,
+	user_name 	VARCHAR2(15)	NOT NULL,
+	password 	VARCHAR2(7)	NOT NULL,
+	age 		NUMBER(3),
+	reg_date	DATE);
+
+INSERT INTO users 
+VALUES('user01','홍길동','user01',10,
+	 TO_DATE( '01/10/2011 01:10:11 오전', 'MM/DD/YYYY HH:MI:SS AM'));
+
+INSERT INTO users 
+VALUES('user02','이순신','user02',20,
+	 TO_DATE( '02/20/2022 02:20:22 오후', 'MM/DD/YYYY HH:MI:SS AM'));
+
+INSERT INTO users 
+VALUES('user03','김유신','user03',30,
+	 TO_DATE( '03/30/2033 03:30:33 오전', 'MM/DD/YYYY HH:MI:SS AM'));
+
+COMMIT;
+
+==============================
+ users TABLE QUERY   
+==============================
+SELECT * FROM users;
+
